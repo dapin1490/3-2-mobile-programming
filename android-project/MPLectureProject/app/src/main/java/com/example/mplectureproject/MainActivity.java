97b5aae5d6b254ca3ef1b33ff6ca7c6b5262ab7f
dapin <89961651+dapin1490@users.noreply.github.com>
2022-09-27 11:09:59 +0900

    220927 in-class

--- android-project/MPLectureProject/app/src/main/java/com/example/mplectureproject/MainActivity.java
+++ android-project/MPLectureProject/app/src/main/java/com/example/mplectureproject/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mplectureproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+//		EditText edit = findViewById(R.id.edit); // 그냥 edit은 텍스트도 아니고 char시퀀스도 아님
+//		String s = edit.getText().toString(); // 토스트에 edit 그대로 쓰면 안 됨
+
+		Button btn = findViewById(R.id.btn);
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				EditText edit = findViewById(R.id.edit);
+				String s = edit.getText().toString();
+
+				Toast.makeText(MainActivity.this, "button pressed", Toast.LENGTH_LONG).show();
+				Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		Toast.makeText(this, "this is a toast", Toast.LENGTH_LONG).show(); // 꼭 뒤에 show를 달아줘야 나타남
 	}
 }
+
+//class Test {
+//	public void foo() {
+//		EditText edit = findViewById(R.id.edit);
+//		String s = edit.getText().toString();
+//	}
+//}
