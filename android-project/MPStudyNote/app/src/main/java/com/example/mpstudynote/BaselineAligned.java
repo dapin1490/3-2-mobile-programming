a59f3a646e60cfdf5284f8eebf8d62e15f5fedfc
dapin <89961651+dapin1490@users.noreply.github.com>
2022-10-15 21:12:16 +0900

    221015 정리

---
+++ android-project/MPStudyNote/app/src/main/java/com/example/mpstudynote/BaselineAligned.java
@@ -0,0 +1,13 @@
+package com.example.mpstudynote;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaselineAligned extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.baseline_aligned);
+    }
+}
---
+++ android-project/MPStudyNote/app/src/main/java/com/example/mpstudynote/RelativeLayout.java
@@ -0,0 +1,13 @@
+package com.example.mpstudynote;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout);
+    }
+}
